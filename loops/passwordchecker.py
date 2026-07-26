5495788a4cbcb1776f55d0f5fdf2ec6b89d60961
Vedhasree Penchala <penchalavedha@gmail.com>
2026-07-26 20:51:44 +0530

    Day 3:Practice programs using loops

---
+++ loops/passwordchecker.py
@@ -0,0 +1,23 @@
+# password=input("Enter your password:")
+# while(True):
+#     if password=="python":
+#         break
+#     else:
+#         print("Incorrect password. Please try again.")
+#         password=input("Enter your password:")
+# print("Access granted")
+
+
+pwd=input("enter your password:")
+# for i in pwd:
+#     if pwd=="python":
+#         break
+#     else:
+#         print("Incorrect password. Please try again.")
+#         pwd=input("enter your password:")
+while(pwd!="python"):
+    # print("password is incorrect,Please try again later /n Enter your password")  infinite loop 
+    print("password is incorrect,Please try again later")
+    pwd=input("enter your password:")
+print("Access granted")
+          
